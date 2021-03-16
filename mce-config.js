97025aa469d6edb725ae072d06b8b4c73f0d9c62
Andreas Marr <monolar@monolar.org>
2021-03-16 23:12:03 +0100

    Add image float styles

--- mce-config.js
+++ mce-config.js
@@ -1,4 +1,28 @@
 Hooks.on("init", () => {            
-		CONFIG.TinyMCE.toolbar = "styleselect forecolor backcolor bullist numlist image table hr link removeformat code fontsizeselect save";
+	CONFIG.TinyMCE.toolbar = "styleselect forecolor backcolor bullist numlist image table hr link removeformat code fontsizeselect save";
+	CONFIG.TinyMCE.style_formats = CONFIG.TinyMCE.style_formats.concat([
+        {
+            title: "Float",
+            items: [
+                {
+                    title: 'Left',
+                    selector: 'img',
+                    styles: {
+                        'float': 'left', 
+                        'margin': '0 10px 0 10px'
+                    }
+                },
+                {
+                    title: 'Right',
+                    selector: 'img', 
+                    styles: {
+                        'float': 'right', 
+                        'margin': '0 0 10px 10px'
+                    }
+                }
+            ]
+        }
+    ]);
+
 });
 
